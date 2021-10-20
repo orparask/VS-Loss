09d1c1a6710afbc73f078c6cfcf99537ce458314
Orestis Paraskevas <orestis@ucsb.edu>
2021-10-19 23:29:25 -0700

    initial commit

---
+++ losses.py
@@ -0,0 +1,84 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BinaryVSLoss(nn.Module):
+
+    def __init__(self, iota_pos=0.0, iota_neg=0.0, Delta_pos=1.0, Delta_neg=1.0, weight=None):
+
+        super(BinaryVSLoss, self).__init__()
+        iota_list = torch.tensor([iota_neg, iota_pos]).to(torch.device('cuda'))
+        Delta_list = torch.tensor([Delta_neg, Delta_pos]).to(torch.device('cuda'))
+
+        self.iota_list = torch.cuda.FloatTensor(iota_list)
+        self.Delta_list = torch.cuda.FloatTensor(Delta_list)
+        self.weight = weight
+
+    def forward(self, x, target):
+
+        index = torch.zeros((x.shape[0], 2), dtype=torch.uint8)
+        index_float = index.type(torch.cuda.FloatTensor)
+        index_float.scatter_(1, target.long(), 1)
+
+        batch_iota = torch.matmul(self.iota_list, index_float.t())
+        batch_Delta = torch.matmul(self.Delta_list, index_float.t())
+
+        batch_iota = batch_iota.view((-1, 1))
+        batch_Delta = batch_Delta.view((-1, 1))
+
+        output = x * batch_Delta - batch_iota
+
+        return F.binary_cross_entropy_with_logits(30*output, target, weight=self.weight)
+
+
+class VSLoss(nn.Module):
+
+    def __init__(self, cls_num_list, gamma=0.3, tau=1.0, weight=None):
+
+        super(VSLoss, self).__init__()
+
+        cls_probs = [cls_num / sum(cls_num_list) for cls_num in cls_num_list]
+        temp = (1.0 / np.array(cls_num_list)) ** gamma
+        temp = temp / np.min(temp)
+
+        iota_list = tau * np.log(cls_probs)
+        Delta_list = temp
+
+        self.iota_list = torch.cuda.FloatTensor(iota_list)
+        self.Delta_list = torch.cuda.FloatTensor(Delta_list)
+        self.weight = weight
+
+    def forward(self, x, target):
+
+        output = x / self.Delta_list + self.iota_list
+
+        return F.cross_entropy(output, target, weight=self.weight)
+
+
+class LDAMLoss(nn.Module):
+
+    def __init__(self, cls_num_list, max_m=0.5, weight=None, s=30):
+        super(LDAMLoss, self).__init__()
+        m_list = 1.0 / np.sqrt(np.sqrt(cls_num_list))
+        m_list = m_list * (max_m / np.max(m_list))
+        m_list = torch.cuda.FloatTensor(m_list)
+        self.m_list = m_list
+        assert s > 0
+        self.s = s
+        self.weight = weight
+
+    def forward(self, x, target):
+        index = torch.zeros_like(x, dtype=torch.uint8)
+        index.scatter_(1, target.data.view(-1, 1), 1)
+
+        index_float = index.type(torch.cuda.FloatTensor)
+        batch_m = torch.matmul(self.m_list[None, :], index_float.transpose(0, 1))
+        batch_m = batch_m.view((-1, 1))
+        x_m = x - batch_m
+
+        # print(x)
+        output = torch.where(index, x_m, x)
+        # print(output)
+        return F.cross_entropy(self.s * output, target, weight=self.weight)
